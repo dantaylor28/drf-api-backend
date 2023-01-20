13e839c6d80dcdd714598e53303b691414ff218a
DanTaylor28 <dan.taylor1493@gmail.com>
2023-01-20 11:35:23 +0000

    Create Pin model and meta class with unique_together relationship

--- pins/models.py
+++ pins/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 from posts.models import Post
+
+
+class Pin(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='pins')
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-timestamp']
+        unique_together = ['owner', 'post']
+
+    def __str__(self):
+        return f"{self.owner} pinned {self.post}"
