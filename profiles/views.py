99c8ac7384aa1f78115471ccc92bde4a86b648ff
DanTaylor28 <dan.taylor1493@gmail.com>
2023-01-20 10:38:05 +0000

    Create ProfileDetailView to retrieve a single users profile

--- profiles/views.py
+++ profiles/views.py
@@ -8,3 +8,7 @@ class ProfileListView(generics.ListAPIView):
     serializer_class = ProfileSerializer
     queryset = Profile.objects.all().order_by('-created_at')
 
+
+class ProfileDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ProfileSerializer
+    queryset = Profile.objects.all()
