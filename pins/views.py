9a20e4720a4a212376d2e654d51faad655818f68
DanTaylor28 <dan.taylor1493@gmail.com>
2023-01-20 11:52:50 +0000

    Create PinListView and define perform_create method

--- pins/views.py
+++ pins/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Pin
+from .serializers import PinSerializer
+from rest_framework import generics
 
-# Create your views here.
+
+class PinListView(generics.ListCreateAPIView):
+    serializer_class = PinSerializer
+    queryset = Pin.objects.all().order_by('-timestamp')
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
