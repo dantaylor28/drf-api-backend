d902a1491e0d1b11463693f84f14ae7b2a8d2950
DanTaylor28 <dan.taylor1493@gmail.com>
2023-01-20 12:06:24 +0000

    Define a create function to handle duplicate pin attempts

--- pins/serializers.py
+++ pins/serializers.py
@@ -1,10 +1,19 @@
 from rest_framework import serializers
 from .models import Pin
+from django.db import IntegrityError
 
 
 class PinSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     post_title = serializers.ReadOnlyField(source='post.title')
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'error': 'you cannot pin a post more than once'
+            })
 
     class Meta:
         model = Pin
