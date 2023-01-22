620834e1951a40ac4df8a6960cafd28462ab59f5
DanTaylor28 <dan.taylor1493@gmail.com>
2023-01-22 17:34:28 +0000

    Create Follower model along with meta and __str__ method

--- followers/models.py
+++ followers/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Follower(models.Model):
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='following')
+    followed = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='followed')
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-timestamp']
+        unique_together = ['owner', 'followed']
+
+    def __str__(self):
+        return f"{self.followed} followed by {self.owner}"
