9c4eaf3528d98de18a94425205b327c993d862fa
DanTaylor28 <dan.taylor1493@gmail.com>
2023-01-22 08:41:11 +0000

    Create comment model and meta class

--- comments/models.py
+++ comments/models.py
@@ -1,2 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from posts.models import Post
 
+
+class Comment(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    text = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-timestamp']
+
+    def __str__(self):
+        return f"{self.owner} commented: {self.text}"
