493def4c8f004ea778c53cc3ba3d2cb9fb9e62ae
DanTaylor28 <dan.taylor1493@gmail.com>
2023-01-24 08:37:05 +0000

    Add project wide default permission class, IsAuthenticatedOrReadOnly to my settings.py file

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -17,6 +17,12 @@ import re
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ]
+}
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
