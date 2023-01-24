d2f00133ac864738c9c185163fc360e01b212ab4
DanTaylor28 <dan.taylor1493@gmail.com>
2023-01-24 10:10:00 +0000

    Create the model for my categories app

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """
+    Category model will store different category names in the
+    database. A category field is added to the Post model via a
+    foreign key to allow posts to be assigned a category upon creation
+    """
+    name = models.CharField(max_length=100, unique=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-timestamp']
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
