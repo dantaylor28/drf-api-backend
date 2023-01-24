9e2702475d79fb246b1f0f7e13f62e597cc508d3
DanTaylor28 <dan.taylor1493@gmail.com>
2023-01-24 19:47:00 +0000

    Add filtering to show comment specific comment likes on CommentLikeListView

--- comment_likes/views.py
+++ comment_likes/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import CommentLike
 from .serializers import CommentLikeSerializer
 from rest_framework import generics
@@ -8,6 +9,10 @@ from drf_api.permissions import IsOwnerOrReadOnly
 class CommentLikeListView(generics.ListCreateAPIView):
     serializer_class = CommentLikeSerializer
     queryset = CommentLike.objects.all().order_by('-timestamp')
+
+    filter_backends = [DjangoFilterBackend]
+
+    filterset_fields = ['comment']
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
