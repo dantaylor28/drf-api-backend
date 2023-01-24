83c94f4c11a32bbace27c274b5e60bf93d808b75
DanTaylor28 <dan.taylor1493@gmail.com>
2023-01-24 08:29:00 +0000

    Add api-auth/ url path to urls.py to allow signin/out functionality

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
 
     path('', include('posts.urls')),
     path('', include('profiles.urls')),
