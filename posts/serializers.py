1a479c99d8a71c118eade35fe446cba590b7530d
DanTaylor28 <dan.taylor1493@gmail.com>
2023-01-24 13:13:14 +0000

    Define get_is_post_owner method and add the field to my PostSerializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -7,10 +7,15 @@ class PostSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(
         source='owner.profile.profile_image.url')
+    is_post_owner = serializers.SerializerMethodField()
+
+    def get_is_post_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
 
     class Meta:
         model = Post
         fields = [
-            'id', 'title', 'caption', 'owner', 'uploaded_at',
-            'updated_at', 'post_image', 'profile_id', 'profile_image',
+            'id', 'title', 'caption', 'owner', 'is_post_owner',
+            'uploaded_at', 'updated_at', 'post_image', 'profile_id', 'profile_image',
         ]
