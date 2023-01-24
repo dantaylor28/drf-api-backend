c0624bbfbbd93a9e5ab5ce4445e7f8472b580384
DanTaylor28 <dan.taylor1493@gmail.com>
2023-01-24 10:30:34 +0000

    Create CategoryListView and CategoryDetailView

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Category
+from .serializers import CategorySerializer
+from rest_framework import generics
+from drf_api.permissions import IsOwnerOrReadOnly
 
-# Create your views here.
+
+class CategoryListView(generics.ListCreateAPIView):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+
+
+class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
