298ae8e0c0817818aef07b73d3e7f9e0b24af57e
DanTaylor28 <dan.taylor1493@gmail.com>
2023-01-24 13:21:06 +0000

    Define get_pinned_id method and add the field to my PostSerializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Post
+from pins.models import Pin
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -8,14 +9,25 @@ class PostSerializer(serializers.ModelSerializer):
     profile_image = serializers.ReadOnlyField(
         source='owner.profile.profile_image.url')
     is_post_owner = serializers.SerializerMethodField()
+    pinned_id = serializers.SerializerMethodField()
 
     def get_is_post_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_pinned_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            pin = Pin.objects.filter(
+                owner=user, post=obj
+            ).first()
+            return pin.id if pin else None
+        return None
+
     class Meta:
         model = Post
         fields = [
             'id', 'title', 'caption', 'owner', 'is_post_owner',
-            'uploaded_at', 'updated_at', 'post_image', 'profile_id', 'profile_image',
+            'uploaded_at', 'updated_at', 'post_image', 'profile_id',
+            'profile_image', 'pinned_id'
         ]
