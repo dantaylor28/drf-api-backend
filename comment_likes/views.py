d1eef96e995feccbbccae6819d6cfa3a5eaf6217
DanTaylor28 <dan.taylor1493@gmail.com>
2023-01-24 09:49:25 +0000

    Create CommentLikeListView and define perform_create function

--- comment_likes/views.py
+++ comment_likes/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import CommentLike
+from .serializers import CommentLikeSerializer
+from rest_framework import generics
+from drf_api.permissions import IsOwnerOrReadOnly
 
-# Create your views here.
+
+class CommentLikeListView(generics.ListCreateAPIView):
+    serializer_class = CommentLikeSerializer
+    queryset = CommentLike.objects.all().order_by('-timestamp')
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
