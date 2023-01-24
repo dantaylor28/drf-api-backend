e7d8c2c9981a335c0d9dc5b0d0450f7b59eb5f7b
DanTaylor28 <dan.taylor1493@gmail.com>
2023-01-24 20:08:23 +0000

    Setup page pagination in settings.py file

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -29,7 +29,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
 
 
