5cce1523ad99e1ed3caffafcc3115be8dc0e4173
DanTaylor28 <dan.taylor1493@gmail.com>
2023-01-24 13:05:27 +0000

    Add profile stats to ProfileDetailView as well as ListView

--- profiles/views.py
+++ profiles/views.py
@@ -21,9 +21,14 @@ class ProfileListView(generics.ListAPIView):
 
 class ProfileDetailView(generics.RetrieveUpdateDestroyAPIView):
     """
-    Lists all information for a selected user and the ability to delete
-    the profile if you are the owner
+    Lists all information for a selected user and the ability to edit
+    and delete the profile if you are the owner
     """
     serializer_class = ProfileSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        num_of_posts=Count('owner__post', distinct=True),
+        num_of_followers=Count('owner__followed', distinct=True),
+        num_of_following=Count('owner__following', distinct=True),
+        num_of_pinned_posts=Count('owner__pin', distinct=True)
+    ).order_by('-created_at')
