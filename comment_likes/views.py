85f3a6ba4d0a2a0d2768462327f2d5b1ae34b844
DanTaylor28 <dan.taylor1493@gmail.com>
2023-01-24 09:51:01 +0000

    Create CommentLikeDetailView

--- comment_likes/views.py
+++ comment_likes/views.py
@@ -11,3 +11,9 @@ class CommentLikeListView(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+
+class CommentLikeDetailView(generics.RetrieveDestroyAPIView):
+    serializer_class = CommentLikeSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = CommentLike.objects.all()
