0f05bc75d8d88d64c4b2b40ce35c38feea49937c
DanTaylor28 <dan.taylor1493@gmail.com>
2023-01-24 10:47:27 +0000

    Create custom permission isAdminOrReadOnly and apply it to categories/views.py

--- categories/views.py
+++ categories/views.py
@@ -2,14 +2,16 @@ from django.shortcuts import render
 from .models import Category
 from .serializers import CategorySerializer
 from rest_framework import generics
-from drf_api.permissions import IsOwnerOrReadOnly
+from drf_api.permissions import IsOwnerOrReadOnly, IsAdminOrReadOnly
 
 
 class CategoryListView(generics.ListCreateAPIView):
     serializer_class = CategorySerializer
+    permission_classes = [IsAdminOrReadOnly]
     queryset = Category.objects.all()
 
 
 class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = CategorySerializer
+    permission_classes = [IsAdminOrReadOnly]
     queryset = Category.objects.all()
--- drf_api/permissions.py
+++ drf_api/permissions.py
@@ -6,3 +6,15 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.owner == request.user
+
+
+class IsAdminOrReadOnly(permissions.IsAdminUser):
+    """
+    Custom permission to ensure only the admin can have access to
+    certain features, primarily creating and editing categories
+    """
+    def has_permission(self, request, view):
+        is_admin = super(
+            IsAdminOrReadOnly,
+            self).has_permission(request, view)
+        return request.method in permissions.SAFE_METHODS or is_admin
