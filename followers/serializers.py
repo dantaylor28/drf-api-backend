2fcd115ed1f81e6a503d08dc7b9628f4ec90e7fc
DanTaylor28 <dan.taylor1493@gmail.com>
2023-01-24 07:37:04 +0000

    Define a followed_user read only field on my FollowerSerializer

--- followers/serializers.py
+++ followers/serializers.py
@@ -4,10 +4,11 @@ from .models import Follower
 
 class FollowerSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    followed_user = serializers.ReadOnlyField(source='followed.username')
 
     class Meta:
         model = Follower
         fields = [
-            'id', 'owner', 'followed',
+            'id', 'owner', 'followed', 'followed_user',
             'timestamp'
         ]
