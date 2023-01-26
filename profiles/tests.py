a40dec7e5ed23c98799f96d21804fddc41271e0e
DanTaylor28 <dan.taylor1493@gmail.com>
2023-01-26 15:40:36 +0000

    Ensure test_update_others_profile fails and then passes as expected

--- profiles/tests.py
+++ profiles/tests.py
@@ -16,3 +16,8 @@ class ProfileDetailViewTests(APITestCase):
         profile = Profile.objects.filter(pk=1).first()
         self.assertEqual(profile.location, 'sweden')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_update_others_profile(self):
+        self.client.login(username='dan', password='password1')
+        response = self.client.put('/profiles/2', {'location': 'sweden'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
