43498bfed7315f138dd5a4efd24a5a520b4a40a7
DanTaylor28 <dan.taylor1493@gmail.com>
2023-01-26 13:44:01 +0000

    Set default renderer to Json while in production

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -41,6 +41,11 @@ REST_FRAMEWORK = {
     )]
 }
 
+if 'DEV' not in os.environ:
+    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
+        'rest_framework.renderers.JSONRenderer',
+    ]
+
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True
 JWT_AUTH_COOKIE = 'my-app-auth'
