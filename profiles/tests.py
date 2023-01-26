8e38208e7cf47b8a87b085263858ada11ffc4776
DanTaylor28 <dan.taylor1493@gmail.com>
2023-01-26 15:34:32 +0000

    Ensure test_update_own_profile fails and then passes successfully

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Profile
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class ProfileDetailViewTests(APITestCase):
+    def setUp(self):
+        dan = User.objects.create_user(username='dan', password='password1')
+        sabina = User.objects.create_user(
+            username='sabina', password='password1')
+
+    def test_update_own_profile(self):
+        self.client.login(username='dan', password='password1')
+        response = self.client.put('/profiles/1', {'location': 'sweden'})
+        profile = Profile.objects.filter(pk=1).first()
+        self.assertEqual(profile.location, 'sweden')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
