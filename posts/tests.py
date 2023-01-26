49168679690b09f9d255e706fcfeab78d6ebe36c
DanTaylor28 <dan.taylor1493@gmail.com>
2023-01-26 14:28:40 +0000

    Create test_list_all_posts and ensure it passes as expected

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Post
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class PostListViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='Dan', password='password1')
+
+    def test_list_all_posts(self):
+        dan = User.objects.get(username='Dan')
+        Post.objects.create(owner=dan, title='test')
+        response = self.client.get('/posts/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
