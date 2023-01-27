4593ad3ec761735863c13b444cd868b78cc285e6
DanTaylor28 <dan.taylor1493@gmail.com>
2023-01-27 13:10:41 +0000

    Ensure test_only_admin_can_make_category passes as it should

--- categories/tests.py
+++ categories/tests.py
@@ -6,10 +6,15 @@ from rest_framework.test import APITestCase
 
 class CategoryListViewTests(APITestCase):
     def setUp(self):
-        dan = User.objects.create_user(username='dan', password='password1')
+        User.objects.create_user(username='dan', password='password1')
 
     def test_list_all_categories(self):
         dan = User.objects.get(username='dan')
         Category.objects.create(name='food')
         response = self.client.get('/categories/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_logged_in_can_make_category(self):
+        self.client.login(username='dan', password='password1')
+        response = self.client.post('/categories/', {'name': 'food'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
