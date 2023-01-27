2ddae7883eec1f3d3f8cb3380cf66c21a2695fa6
DanTaylor28 <dan.taylor1493@gmail.com>
2023-01-27 10:19:59 +0000

    Ensure test_list_all_comments fails and then passes as expected

--- comments/tests.py
+++ comments/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Comment, Post
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class CommentListViewTests(APITestCase):
+    def setUp(self):
+        dan = User.objects.create_user(username='dan', password='password1')
+
+    def test_list_all_comments(self):
+        dan = User.objects.get(username='dan')
+        post1 = Post.objects.create(owner=dan, title='dans post')
+        Comment.objects.create(owner=dan, post=post1)
+        response = self.client.get('/comments/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
