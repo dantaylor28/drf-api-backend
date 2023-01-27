96815c0f99cdb54dc3491c9ea74845536e542511
DanTaylor28 <dan.taylor1493@gmail.com>
2023-01-27 07:47:37 +0000

    Ensure test_list_all_pins fails and then passes as expected

--- pins/tests.py
+++ pins/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Pin
+from .models import Post
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class PinListViewTests(APITestCase):
+    def setUp(self):
+        dan = User.objects.create_user(username='dan', password='password1')
+
+    def test_list_all_pins(self):
+        dan = User.objects.get(username='dan')
+        test = Post.objects.create(owner=dan, title='dans post')
+        Pin.objects.create(owner=dan, post=test)
+        response = self.client.get('/pins/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
